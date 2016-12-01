ba3b13c0a425ca499289127ad831ae2d44f87a73
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2016-12-01 07:56:09 -0300

    - Finish ALU modules simulation.

--- toplevel/datapath/modules/alu/modules/tb.sv
+++ toplevel/datapath/modules/alu/modules/tb.sv
@@ -25,18 +25,19 @@ module tb();
       #100ns;
 		//Decode ADD again
       f = 2'h0;
+      #50ns;
    end
 
 	//Logical Unit
 	logic [NBITS-1:0] a, b, y_lu;
 	logic [2:0] lu_ctrl;
-	
+
    logical_unit lu_i(.a(a), .b(b), .ctrl(lu_ctrl), .y(y_lu));
 
    initial begin
       #1ns;
-		a = 'hAAAAAAAA;
-		b = 'hBBBBBBBB;
+		a = 'h0000AAAA;
+      b = 'hBBBB0000;
       //Do AND
       lu_ctrl = 3'b001;
       #100ns;
@@ -44,6 +45,8 @@ module tb();
       lu_ctrl = 3'b010;
       #100ns;
       //Do NOTB
+		a = 'hAAAAAAAA;
+		b = 'hBBBBBBBB;
       lu_ctrl = 3'b100;
       #100ns;
       //Do NOP
@@ -51,8 +54,30 @@ module tb();
       #100ns;
 		//Do OR again
       lu_ctrl = 3'b010;
+      #50ns;
    end
-	
+
+   //Full Adder
+   logic enable;
+   logic c_in;
+   logic [NBITS-1:0] y_fa;
+   logic c_out;
+
+   full_adder fa_i(.enable(enable), .c_in(c_in), .a(a), .b(b), .y(y_fa), .c_out(c_out));
+
+   initial begin
+      #1ns;
+      enable = 1'b1;
+      c_in = 1'b0;
+      #100ns;
+      c_in = 1'b1;
+      #200ns;
+      c_in = 1'b0;
+      #100ns;
+      enable = 1'b0;
+      #50ns;
+   end
+
    initial begin
      $dumpfile ("waves.vcd");
      $dumpvars (1, tb);

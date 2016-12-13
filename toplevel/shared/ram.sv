0aa0b2bef68be48e56676be4667de6e431815591
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2016-12-12 21:52:19 -0300

    - Put clock in the interface of RAM bus.

--- toplevel/shared/ram.sv
+++ toplevel/shared/ram.sv
@@ -1,20 +1,20 @@
 `include "definitions.svh"
 
-interface ram_bus ();
+interface ram_bus (input logic clk);
 	logic we;
 	logic [BYTE-1:0] data, q;
 	logic [NBITS-1:0] addr;
 
-	modport slave (input we, data, addr, output q);
-	modport master (input q, output we, data, addr);
+	modport slave (input clk, we, data, addr, output q);
+	modport master (input clk, q, output we, data, addr);
 endinterface
 
-module ram (input logic clk, ram_bus.slave bus);
+module ram (ram_bus.slave bus);
 
 	// Single-port Write, Dual-port Read Memory
 	logic [BYTE-1:0] mem [0:WORDS-1];
 
-	always_ff @ (posedge clk) begin
+	always_ff @ (posedge bus.clk) begin
 		if(bus.we)
 			mem[bus.addr] <= bus.data;
 		bus.q <= mem[bus.addr];
